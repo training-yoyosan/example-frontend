d3005da63409c5da4e9492d517e4d6617ad8f571
Yoyo San <veselu@gmail.com>
2020-07-22 19:12:38 +0300

    feat: Add configuration for axios and env

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,4 +1,7 @@
-import Vue from 'vue'
-import axios from 'axios'
+import Vue from "vue";
+import axios from "axios";
 
-Vue.prototype.$axios = axios
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = process.env.API_BASE_URL;
+
+Vue.prototype.$axios = axios;
